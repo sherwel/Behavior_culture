7316f9fd80eb17272971ecedd2940755e3f69524
nanshihui <nanshihui@qq.com>
2016-02-06 14:25:10 +0800

    修改界面样式

--- Back_ground/views.py
+++ Back_ground/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django import forms
 # Create your views here.
+class UploadForm(forms.Form):
+    filename=forms.CharField()
+    filedetail=forms.FileField()
