86947d81ef790b671f985574b1a29224addb79ea
nanshihui <nanshihui@qq.com>
2016-02-16 18:29:13 +0800

    add login info

--- Back_ground/xlsdeal/xls_opertate/student_operate.py
+++ Back_ground/xlsdeal/xls_opertate/student_operate.py
@@ -20,12 +20,14 @@ def loadstudent(path):
     data=xlstool.getdata(path, u'学生')
     studentdata=[]
     studentattenddata=[]
+    logindata=[]
     for i in data:
         temp=list(i)
         studentid=uuid.uuid1()
 #         temp.append('classid')
         studentdata.append((studentid,temp[0],temp[1],temp[2],temp[3],temp[4],temp[5],temp[6],temp[7],temp[8],temp[9],temp[10]))
         studentattenddata.append((studentid,temp[11]))
+        logindata.append((temp[12],'e10adc3949ba59abbe56e057f20f883e',studentid))
 
           
     sqlhelp=SQLTool.getObject()
@@ -33,7 +35,9 @@ def loadstudent(path):
     result=sqlhelp.inserttableinfo_byparams(config.Config.studenttable, ['studentId','name','sex','birthday','address','familyPhone','mail','family','admissionTime','nation','placeOfOrigin','team'], studentdata)
     if result:
         result=sqlhelp.inserttableinfo_byparams(config.Config.student_attend_table, ['studentId','classId'], studentattenddata)
- 
+        if result:
+            result=sqlhelp.inserttableinfo_byparams(config.Config.usertable, ['username','password','userId'], logindata)
+  
     return result
 
 
